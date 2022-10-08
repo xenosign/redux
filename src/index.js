6059b43f3a147539360d12b7f643cb12957cd815
kdtTetz <xenosign@naver.com>
2022-10-08 19:19:25 +0900

    redux 를 configureStore 로 변경

--- src/index.js
+++ src/index.js
@@ -4,15 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { createStore } from 'redux';
 import rootReducer from './store';
-
 import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
 
 const reduxDevTool =
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
-const store = createStore(rootReducer, reduxDevTool);
+const store = configureStore({ reducer: rootReducer }, reduxDevTool);
 console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
